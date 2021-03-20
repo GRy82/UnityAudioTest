8f2c5570837ceb26857eacc87ac27e98e633cc32
Gregory Ryan <gwryan@buffalo.edu>
2021-03-20 10:57:25 -0400

    Audio Sources added
    Audio sources added to scene, including a moving audio source.  The audio sources use 3D logarithmic rolloff to diminish sound with distance from the listener.

---
+++ Assets/WalkPastWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkPastWindow : MonoBehaviour
+{
+    public Vector3 walkingSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localPosition += walkingSpeed;
+    }
+}
